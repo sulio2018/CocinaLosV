1801219e25142a239a8abfc48730b550b5921871
jmartinez <jcmartinez1925@gmail.com>
2020-03-25 12:04:35 -0700

    add hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import React, { Component } from 'react';
+import { HashRouter, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -9,26 +10,38 @@ import Catering from "./pages/Catering";
 import Contact from "./pages/Contact";
 import "./App.css"
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
 
-    <div className='page-container'>
-      <div className='content-wrap'>
-        <Router>
-          <Navbar />
-          <Switch>
-            <Route exact path="/"> <Home /> </Route>
-            <Route exact path="/about"> <About /> </Route>
-            <Route exact path="/foodstand"> <FoodStand /> </Route>
-            <Route exact path="/catering"> <Catering /> </Route>/>
-            <Route exact path="/contact"> <Contact /> </Route> />
-          </Switch>
-        </Router>
+      <div className='page-container'>
+        <div className='content-wrap'>
+          <HashRouter basename="/">
+            <Navbar />
+            <div>
+
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/foodstand">Foodstand</Link></li>
+                <li><Link to="/catering">Catering</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+              </ul>
+
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/foodstand" component={FoodStand} />
+              <Route exact path="/catering" component={Catering} />
+              <Route exact path="/contact" component={Contact} />
+
+            </div>
+            <Footer />
+          </HashRouter>
+        </div>
       </div>
-      <Footer />
-    </div>
 
-  );
+    );
+  }
 }
 
 export default App;
