bd5fa6c8a70f1ef74c148e4437dd5efb7c67710e
jmartinez <jcmartinez1925@gmail.com>
2020-03-24 22:37:34 -0700

    Updates

---
+++ precache-manifest.322f24a63ff90d3265764a584faca83d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790009777b48a64b6d659da234b82af6",
+    "url": "/cocinalosv/index.html"
+  },
+  {
+    "revision": "e4bf9521995458a8d6bf",
+    "url": "/cocinalosv/static/css/2.e49e09ab.chunk.css"
+  },
+  {
+    "revision": "1f2eb5b65cb45fe2f7b5",
+    "url": "/cocinalosv/static/css/main.f456df29.chunk.css"
+  },
+  {
+    "revision": "e4bf9521995458a8d6bf",
+    "url": "/cocinalosv/static/js/2.5c88c0b1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cocinalosv/static/js/2.5c88c0b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2eb5b65cb45fe2f7b5",
+    "url": "/cocinalosv/static/js/main.11ba0374.chunk.js"
+  },
+  {
+    "revision": "dc7fe83296635d8df4e0",
+    "url": "/cocinalosv/static/js/runtime-main.a449a04a.js"
+  },
+  {
+    "revision": "dc83f005a313370305da171682764c2e",
+    "url": "/cocinalosv/static/media/about2.dc83f005.jpg"
+  },
+  {
+    "revision": "8513be2507b64e77193d1440e1100669",
+    "url": "/cocinalosv/static/media/catering1.8513be25.jpg"
+  },
+  {
+    "revision": "d7b8f4e471f9d98cef43f8ce200036c2",
+    "url": "/cocinalosv/static/media/catering3.d7b8f4e4.jpg"
+  },
+  {
+    "revision": "5c810c843c2c572e870ad7bf59e3c099",
+    "url": "/cocinalosv/static/media/catering5.5c810c84.jpg"
+  },
+  {
+    "revision": "869fa017eec3e96ff7fb076ce708d1eb",
+    "url": "/cocinalosv/static/media/foodstand1.869fa017.jpg"
+  },
+  {
+    "revision": "3399dca8ff4744481b9b6aab5e626dca",
+    "url": "/cocinalosv/static/media/img1.3399dca8.jpg"
+  },
+  {
+    "revision": "b96b89b6b0254daf200d0df72d8be8fd",
+    "url": "/cocinalosv/static/media/img2.b96b89b6.jpg"
+  },
+  {
+    "revision": "7c7eb5bdfc195c4e8b8e854fba8e8c2c",
+    "url": "/cocinalosv/static/media/img3.7c7eb5bd.jpg"
+  },
+  {
+    "revision": "20d51bec595a5bf85ebb2cd4cf08ad2c",
+    "url": "/cocinalosv/static/media/img4.20d51bec.jpg"
+  },
+  {
+    "revision": "6557d5b678c059cb90548aa319c99635",
+    "url": "/cocinalosv/static/media/img5.6557d5b6.jpg"
+  },
+  {
+    "revision": "86ab1cf5f4f58afcdea8064fecba63a8",
+    "url": "/cocinalosv/static/media/img6.86ab1cf5.jpg"
+  },
+  {
+    "revision": "8d78f74708fb9e14e3cbdfa629dd9839",
+    "url": "/cocinalosv/static/media/img7.8d78f747.jpg"
+  },
+  {
+    "revision": "1c8efa2febda0fd36eb5327589d57ac9",
+    "url": "/cocinalosv/static/media/logo.1c8efa2f.png"
+  },
+  {
+    "revision": "772f51be75726aabb9d0269a909b04b9",
+    "url": "/cocinalosv/static/media/video1.772f51be.mp4"
+  }
+]);
